07b65d9ff6e39122ee823c13d701be135762c280
Terje Io <terjeio@users.noreply.github.com>
2023-12-16 16:15:29 +0100

    Added laser plugins.

--- grblHAL_Due/src/my_machine.h
+++ grblHAL_Due/src/my_machine.h
@@ -34,30 +34,32 @@
 // Configuration
 // Uncomment to enable.
 
-//#define USB_SERIAL_CDC     1 // Use native USB port for communication.
-//#define SAFETY_DOOR_ENABLE 1 // Enable safety door input.
-//#define BLUETOOTH_ENABLE   2 // Set to 2 for HC-05 module. Requires and claims one auxillary input pin.
+//#define USB_SERIAL_CDC       1 // Use native USB port for communication.
+//#define SAFETY_DOOR_ENABLE   1 // Enable safety door input.
+//#define BLUETOOTH_ENABLE     2 // Set to 2 for HC-05 module. Requires and claims one auxillary input pin.
 // Spindle selection:
 // Up to four specific spindle drivers can be instantiated at a time
 // depending on N_SPINDLE and N_SYS_SPINDLE definitions in grbl/config.h.
 // If none are specified the default PWM spindle is instantiated.
 // Spindle definitions can be found in grbl/spindle_control.h.
 // More here https://github.com/grblHAL/Plugins_spindle
-//#define SPINDLE0_ENABLE        SPINDLE_HUANYANG1
-//#define SPINDLE1_ENABLE        SPINDLE_PWM0
-//#define SPINDLE2_ENABLE        SPINDLE_NONE 
-//#define SPINDLE2_ENABLE        SPINDLE_NONE
+//#define SPINDLE0_ENABLE      SPINDLE_HUANYANG1
+//#define SPINDLE1_ENABLE      SPINDLE_PWM0
+//#define SPINDLE2_ENABLE      SPINDLE_NONE
+//#define SPINDLE2_ENABLE      SPINDLE_NONE
 // **********************
-//#define MODBUS_ENABLE      1 // Set to 1 for auto direction, 2 for direction signal on auxillary output pin.
-//#define PLASMA_ENABLE      1 // Plasma/THC plugin.
-//#define OPENPNP_ENABLE     1 // OpenPNP plugin. To be completed.
-//#define MPG_ENABLE         1 // Enable MPG interface. Requires serial port and one handshake pin unless
-                               // KEYPAD_ENABLE is set to 2 when mode switching is done by the CMD_MPG_MODE_TOGGLE (0x8B)
-                               // command character. Set both MPG_ENABLE and KEYPAD_ENABLE to 2 to use a handshake pin anyway.
-//#define KEYPAD_ENABLE      2 // Set to 1 for I2C keypad, 2 for other input such as serial data. If KEYPAD_ENABLE is set to 2
-                               // and MPG_ENABLE is uncommented then a serial stream is shared with the MPG.
-//#define EEPROM_ENABLE     16 // I2C EEPROM/FRAM support. Set to 16 for 2K, 32 for 4K, 64 for 8K, 128 for 16K and 256 for 16K capacity.
-//#define EEPROM_IS_FRAM     1 // Uncomment when EEPROM is enabled and chip is FRAM, this to remove write delay.
+//#define MODBUS_ENABLE        1 // Set to 1 for auto direction, 2 for direction signal on auxillary output pin.
+//#define PLASMA_ENABLE        1 // Plasma/THC plugin.
+//#define OPENPNP_ENABLE       1 // OpenPNP plugin. To be completed.
+//#define MPG_ENABLE           1 // Enable MPG interface. Requires serial port and one handshake pin unless
+                                 // KEYPAD_ENABLE is set to 2 when mode switching is done by the CMD_MPG_MODE_TOGGLE (0x8B)
+                                 // command character. Set both MPG_ENABLE and KEYPAD_ENABLE to 2 to use a handshake pin anyway.
+//#define KEYPAD_ENABLE        2 // Set to 1 for I2C keypad, 2 for other input such as serial data. If KEYPAD_ENABLE is set to 2
+                                 // and MPG_ENABLE is uncommented then a serial stream is shared with the MPG.
+//#define LASER_COOLANT_ENABLE 1 // Laser coolant plugin. To be completed.
+//#define LB_CLUSTERS_ENABLE   1 // LaserBurn cluster support.
+//#define EEPROM_ENABLE       16 // I2C EEPROM/FRAM support. Set to 16 for 2K, 32 for 4K, 64 for 8K, 128 for 16K and 256 for 16K capacity.
+//#define EEPROM_IS_FRAM       1 // Uncomment when EEPROM is enabled and chip is FRAM, this to remove write delay.
 #ifndef USB_SERIAL_CDC
 #define SERIAL_DEVICE       -1 // Select serial device for output if not using native USB, default is -1, max value is 2
 #endif
